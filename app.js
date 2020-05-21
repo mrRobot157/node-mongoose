220ecf21353293db976c495e10dd8eb4dc3715ff
Ion Harea <vaniusha.harea.1997@mail.ru>
2020-05-22 01:16:09 +0300

    Express REST API with Mongoose Part 1

--- app.js
+++ app.js
@@ -6,9 +6,22 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const dishRouter =  require('./routes/dishRouter')
-const leaderRouter =  require('./routes/leaderRouter')
-const promoRouter =  require('./routes/promoRouter')
+const dishRouter = require('./routes/dishRouter')
+const leaderRouter = require('./routes/leaderRouter')
+const promoRouter = require('./routes/promoRouter')
+
+const mongoose = require('mongoose')
+
+const Dishes = require('./models/dishes')
+
+const url = 'mongodb://localhost:27017/conFusion'
+const connect = mongoose.connect(url)
+  .then((db) => {
+    console.log('Connected correctly to server');
+  }, (err) => {
+    console.log(err);
+  })
+
 
 var app = express();
 
@@ -28,12 +41,12 @@ app.use('/dishes', dishRouter)
 app.use('/leaders', leaderRouter)
 app.use('/promotions', promoRouter)
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
